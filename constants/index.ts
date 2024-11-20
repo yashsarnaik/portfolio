e1bb2816bdded8a6ddec2623b75ff17cc395108b
yash sarnaik <yashsarnaik2303@gmail.com>
2024-11-20 16:28:27 +0530

    minor

--- constants/index.ts
+++ constants/index.ts
@@ -1,10 +1,9 @@
-import { FaYoutube,FaLinkedin } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 import { FaXTwitter,FaSnapchat  } from "react-icons/fa6";
 import {
   RxDiscordLogo,
   RxGithubLogo,
   RxInstagramLogo,
-  RxTwitterLogo,
   RxLinkedinLogo,
 } from "react-icons/rx";
 
